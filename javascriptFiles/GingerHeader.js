df4251bc1f4fb68d92b3d9f2b92eb640b986f508
gingerjournal <linpeijiang6@gmail.com>
2025-09-28 12:11:15 -0400

    Mobile menu done

---
+++ javascriptFiles/GingerHeader.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.right = '0'
+}
+
+function closeSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.right = '-280px'
+}
